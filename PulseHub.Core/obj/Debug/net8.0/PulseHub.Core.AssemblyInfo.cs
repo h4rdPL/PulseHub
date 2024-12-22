15d8335de635abc22ef47f4088e95a8d59c92427
Mateusz Mamica <mateusz.mamica18@gmail.com>
2024-12-22 11:10:18 +0100

    setup tests for project 🧬

--- PulseHub.Core/obj/Debug/net8.0/PulseHub.Core.AssemblyInfo.cs
+++ PulseHub.Core/obj/Debug/net8.0/PulseHub.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PulseHub.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5916ca5a6f2fc48ea03530a02042bf967b9d878")]
 [assembly: System.Reflection.AssemblyProductAttribute("PulseHub.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PulseHub.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ PulseHub.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PulseHub.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
